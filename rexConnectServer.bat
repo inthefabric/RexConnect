ffd2e77422eac528586cdf81bcd971427d50c343
Zach Kinstner <zach@aestheticinteractive.com>
2013-04-18 23:16:28 -0400

    Included server/console scripts.
    These scripts should be included in the "bin" directory when deploying
    RexConnect.

---
+++ rexConnectServer.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set work=%CD%
+
+if [%work:~-3%]==[bin] cd ..
+
+set LIBDIR=lib
+
+set JAVA_OPTIONS=-Xms32m -Xmx512m
+
+:: Launch the application
+java %JAVA_OPTIONS% %JAVA_ARGS% -cp %LIBDIR%/*; com.fabric.rexconnect.RexConnectServer %*
