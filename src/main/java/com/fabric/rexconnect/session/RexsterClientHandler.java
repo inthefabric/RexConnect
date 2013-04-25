7c6f285e1ece9f00d459278574e9924afb870bbc
zachkinstner <zach@aestheticinteractive.com>
2013-04-25 16:11:54 -0400

    Started experimental "session" branch.
    Using code from @spmallette
    (https://github.com/tinkerpop/rexster/tree/master/rexster-protocol/src/m
    ain/java/com/tinkerpop/rexster/client) as a starting point.

---
+++ src/main/java/com/fabric/rexconnect/session/RexsterClientHandler.java
@@ -0,0 +1,29 @@
+package com.fabric.rexconnect.session;
+
+import com.tinkerpop.rexster.protocol.msg.RexProMessage;
+import org.apache.log4j.Logger;
+import org.glassfish.grizzly.filterchain.BaseFilter;
+import org.glassfish.grizzly.filterchain.FilterChainContext;
+import org.glassfish.grizzly.filterchain.NextAction;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Stephen Mallette (http://stephen.genoprime.com)
+ */
+class RexsterClientHandler extends BaseFilter {
+    private static final Logger logger = Logger.getLogger(RexsterClientHandler.class);
+
+    public NextAction handleRead(FilterChainContext ctx) throws IOException {
+        try {
+            RexsterClient.putResponse((RexProMessage) ctx.getMessage());
+        }
+        catch (Exception e) {
+            logger.error("RexProMessage could not be cast to to be place on the response map.", e);
+        }
+
+        return ctx.getStopAction();
+    }
+}
