1d1d0585499b12ba3fce368a9fbeda8aefd89420
Zach Kinstner <zach@aestheticinteractive.com>
2013-05-29 16:07:29 -0400

    Improved standalone/distribution packaging. Updated ignore file.

--- src/main/bin/rexConnectServer.bat
+++ src/main/bin/rexConnectServer.bat
@@ -4,9 +4,7 @@ set work=%CD%
 
 if [%work:~-3%]==[bin] cd ..
 
-set LIBDIR=lib
-
 set JAVA_OPTIONS=-Xms32m -Xmx512m
 
 :: Launch the application
-java %JAVA_OPTIONS% %JAVA_ARGS% -cp %LIBDIR%/*; com.fabric.rexconnect.main.RexConnectServer %*
+java %JAVA_OPTIONS% %JAVA_ARGS% -cp .\lib\*; com.fabric.rexconnect.main.RexConnectServer %*
